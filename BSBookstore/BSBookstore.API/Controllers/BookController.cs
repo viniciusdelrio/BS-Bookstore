b1815b28bad3a5fc6fbe025f592bace668b863bc
vinicius.delrio <vinicius.delrio41@gmail.com>
2018-03-28 20:48:44 -0300

    - Adicionando a estrutura inicial do projeto; - Adicionado Autofac como DI;

---
+++ BSBookstore/BSBookstore.API/Controllers/BookController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BSBookstore.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+        #region Methods
+
+        #endregion
+    }
+}
---
+++ BSBookstore/BSBookstore.Infrastructure.IoC/Autofac/AutofacConfig.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using System;
+
+namespace BSBookstore.Infrastructure.IoC
+{
+    public static class AutofacConfig
+    {
+        public static IServiceProvider Config()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterAssemblyTypes(AppDomain.CurrentDomain.GetAssemblies())
+                   .AsImplementedInterfaces();
+
+            var container = builder.Build();
+
+            return new AutofacServiceProvider(container);
+        }
+    }
+}
